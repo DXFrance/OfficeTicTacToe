17a02c34135d55c6cfc2725fb53149712a4b3061
Sébastien PERTUS <spertus@microsoft.com>
2016-02-05 12:33:09 -0800

    initial commit

---
+++ OfficeTicTacToe/Views/NextMeetings.xaml.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.OData.Client;
+using OfficeTicTacToe.Common;
+using OfficeTicTacToe.Graph;
+using OfficeTicTacToe.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Animation;
+using System.Diagnostics;
+using System.Threading;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Text;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace OfficeTicTacToe.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class OfficeTicTacToes : BasePage, IRefreshPage
+    {
+        public override string Title
+        {
+            get
+            {
+                return "next meetings";
+            }
+        }
+        public OfficeTicTacToes()
+        {
+            this.InitializeComponent();
+         
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+      
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            using (this.TokenSource = new CancellationTokenSource())
+            {
+                try
+                {
+                    this.IsRefreshButtonEnabled = false;
+                    await Refresh(this.TokenSource.Token, false);
+
+                    
+                }
+                catch (OperationCanceledException ex)
+                {
+                    Debug.WriteLine("Operation canceled " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    this.IsRefreshButtonEnabled = true;
+                }
+
+            }
+
+        }
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+        }
+        public async Task Refresh()
+        {
+            using (this.TokenSource = new CancellationTokenSource())
+            {
+                try
+                {
+                    this.IsRefreshButtonEnabled = false;
+                    await Refresh(this.TokenSource.Token, true);
+                }
+                catch (OperationCanceledException ex)
+                {
+                    Debug.WriteLine("Operation canceled " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    this.IsRefreshButtonEnabled = true;
+                }
+
+            }
+        }
+        public async Task Refresh(CancellationToken token, bool forceRefresh = false)
+        {
+           
+
+            this.SetTitle("loading next meetings ...");
+
+            StackPanelLoader.Visibility = Visibility.Visible;
+            StackPanelLoader.Opacity = 1.0d;
+
+            ProgressRingLoader.Visibility = Visibility.Visible;
+        
+            ProgressRingLoader.IsActive = true;
+
+        
+
+
+            DoubleAnimation animationOpacity = new DoubleAnimation();
+            animationOpacity.To = 0.0d;
+            animationOpacity.From = 1.0d;
+            Storyboard.SetTarget(animationOpacity, StackPanelLoader);
+            Storyboard.SetTargetProperty(animationOpacity, "Opacity");
+
+            DoubleAnimation animationOpacity2 = new DoubleAnimation();
+            animationOpacity2.To = 1.0d;
+            animationOpacity2.From = 0.0d;
+        //    Storyboard.SetTarget(animationOpacity2, EventViewList);
+            Storyboard.SetTargetProperty(animationOpacity2, "Opacity");
+
+            Storyboard sb = new Storyboard();
+            sb.Children.Add(animationOpacity);
+            sb.Children.Add(animationOpacity2);
+
+            sb.Duration = animationOpacity.Duration = animationOpacity2.Duration = TimeSpan.FromMilliseconds(150);
+
+            sb.Completed += (s, o) =>
+            {
+
+                ProgressRingLoader.IsActive = false;
+                ProgressRingLoader.Visibility = Visibility.Collapsed;
+                StackPanelLoader.Visibility = Visibility.Collapsed;
+                AppShell.Current.SetTitle("next meetings");
+
+            };
+
+             sb.Begin();
+
+        }
+       
+
+    }
+
+
+}
